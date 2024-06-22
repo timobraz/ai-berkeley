d61dafba90b0cdda9591a3a135d1c63a2282ce10
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-06-22 14:55:33 -0700

    upload page

--- python/index.py
+++ python/index.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from pypdf import PdfReader
+from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello  World "}
+
+
+@app.post("/pdf")
+async def pdf(
+    file: Annotated[bytes, File()],
+):
+    binary_file = open("my_file.pdf", "wb")
+    binary_file.write(file)
+    reader = PdfReader("my_file.pdf")
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    binary_file.close()
+    return {"text": text}
