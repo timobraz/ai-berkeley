d641e87de3c3d41132c981a68d8902f606586738
Andrew Hwang <pandy430@gmail.com>
2024-06-22 17:34:16 -0700

    Add Navbar component and update dashboard page

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <nav className="absolute w-full p-9 px-[55px] bg-transparent flex items-center justify-between">
+      {/* set div to the very right */}
+      <div className="flex items-center justify-between flex-row w-full">
+        {/* <Button variant="link" className=' font-black'>
+                    <label htmlFor="file-input" className="cursor-pointer">
+                        Upload
+                    </label>
+                    <input id="file-input" type="file" onChange={handleFileChange} className="hidden"></input>
+                </Button> */}
+        <Link href="/" className="mb-4 text-lg font-bold">
+          Environment
+        </Link>
+        <h2 className="mb-4 text-lg font-bold">Upload</h2>
+      </div>
+    </nav>
+  );
+};
