95043f7f2829f1c4f23de980e24ac040c345b5f5
Andrew Hwang <pandy430@gmail.com>
2024-06-22 18:13:57 -0700

    Update dashboard and home page styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,14 +4,20 @@ import { Navbar } from "../components/Navbar";
 
 export default function dashboard() {
   return (
-    <div className="h-[2000px] w-full bg-white bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px]">
+    <div className="h-[1907px] w-full bg-white bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px]">
       {" "}
       <Navbar />
       <div>
         {/* <div className="absolute h-40 w-40 top-1/4 left-1/4 bg-gradient-radial from-teal-400 via-blue-500 to-purple-600 rounded-full mix-blend-multiply filter blur-3xl opacity-80 animate-blob animation-delay-2000 z-0 animation-move-1"></div>
             <div className="absolute h-40 w-40 top-2/3 left-3/4 bg-gradient-radial from-pink-500 via-purple-600 to-indigo-700 rounded-full mix-blend-multiply filter blur-3xl opacity-80 animate-blob animation-delay-2000 z-0 animation-move-2"></div>
             <div className="absolute h-40 w-40 top-3/4 left-1/3 bg-gradient-radial from-amber-400 via-red-500 to-pink-600 rounded-full mix-blend-multiply filter blur-3xl opacity-80 animate-blob animation-delay-2000 z-0 animation-move-3"></div> */}
-        <div className=" w-[92%] left-[4%] h-[850px] relative top-[100px]">
+        <div className="w-[92%] left-[4%] h-[850px] relative top-[100px]">
+          <div className="w-[40%] h-[850px] right-[4%] bg-blue-100 fixed top-[10%] rounded-md"></div>
+          <div className="w-[52%] h-[40%] left-0 bg-green-100 absolute top-0 rounded-md"></div>
+          <div className="w-[25%] h-[55%] right-[48%] bg-purple-100 absolute top-[45%] rounded-md"></div>
+          <div className="w-[24%] h-[55%] left-0 bg-yellow-100 absolute top-[45%] rounded-md"></div>
+        </div>
+        <div className=" w-[92%] left-[4%] h-[850px] relative top-[100px] mt-12">
           <div className="w-[40%] h-[850px] right-[4%] bg-blue-100 fixed top-[10%] rounded-md"></div>
           <div className="w-[52%] h-[40%] left-0 bg-green-100 absolute top-0 rounded-md"></div>
           <div className="w-[25%] h-[55%] right-[48%] bg-purple-100 absolute top-[45%] rounded-md"></div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,17 @@ export default function Home() {
         <div className="relative flex  items-center justify-center space-y-5 h-full bg-green-dots rounded-xl">
           <div className="pb-8 w-full max-w-2xl space-y-4 ">
             <div className="py-8 space-y-2 text-left">
-              <h1 className="mb-4 text-4xl font-light text-white inset-6">Transforming ESG Insights into Action </h1>
+              <h1 className="mb-4 text-4xl font-light text-white inset-6">
+                Transforming ESG Insights into Action{" "}
+              </h1>
               <div className="flex justify-between items-center">
                 <p className="text-gray-200  max-w-[75%] text-xl">
-                  Input an ESG (Environmental, Social, and Governance) report for analysis and to talk to a chatbot.
+                  Input an ESG (Environmental, Social, and Governance) report
+                  for analysis and to talk to a chatbot.
                 </p>
-                <button className="bg-white px-8 py-4 rounded-3xl text-sm">try it out</button>
+                <button className="bg-white px-8 py-4 rounded-3xl text-sm">
+                  try it out
+                </button>
               </div>
             </div>
             <Image
@@ -29,17 +34,24 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="h-screen flex px-32 flex-col gap-8 snap-start py-32">
+      <div className="h-screen flex px-32 flex-col gap-8 bg-white bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px] snap-start py-32">
         <div className="flex w-full items-center flex-col gap-4">
           <h1 className="text-7xl  font-normal">Generate Insights With</h1>
-          <h4 className="text-xl text-gray-500 font-medium">Search a company or upload an ESG report</h4>
+          <h4 className="text-xl text-gray-500 font-medium">
+            Search a company or upload an ESG report
+          </h4>
         </div>
         <div className="bg-green-dots w-full flex flex-col items-center py-16 rounded-xl">
           <div className="flex flex-col gap-2 relative -top-4">
             <h1 className="text-white  text-2xl">Search a company</h1>
             <div className="flex gap-4 items-center justify-between h-14">
-              <input type="text" className="p-2 px-6 text-xl w-[24rem] h-full rounded-md b-2 outline-black outline-2 outline " />
-              <button className="px-10 py-2 font-light text-xl h-full bg-white rounded-md outline-black outline-2 outline">Next</button>
+              <input
+                type="text"
+                className="p-2 px-6 text-xl w-[24rem] h-full rounded-md b-2 outline-black outline-2 outline "
+              />
+              <button className="px-10 py-2 font-light text-xl h-full bg-white rounded-md outline-black outline-2 outline">
+                Next
+              </button>
             </div>
           </div>
         </div>
@@ -47,8 +59,13 @@ export default function Home() {
           <div className="flex flex-col gap-2 relative -top-4">
             <h1 className="text-white  text-2xl">Upload a File</h1>
             <div className="flex gap-4 items-center justify-between h-14">
-              <input type="text" className="p-2 px-6 text-xl w-[24rem] h-full rounded-md b-2 outline-black outline-2 outline " />
-              <button className="px-10 py-2 font-light text-xl h-full bg-white rounded-md outline-black outline-2 outline">Next</button>
+              <input
+                type="text"
+                className="p-2 px-6 text-xl w-[24rem] h-full rounded-md b-2 outline-black outline-2 outline "
+              />
+              <button className="px-10 py-2 font-light text-xl h-full bg-white rounded-md outline-black outline-2 outline">
+                Next
+              </button>
             </div>
           </div>
         </div>
