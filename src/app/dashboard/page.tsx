b710b85925900463f0a752567455176995d1547e
Andrew Hwang <pandy430@gmail.com>
2024-06-22 20:32:59 -0700

    Add chat messages to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,19 @@ import React, { useState } from "react";
 import { Navbar } from "../components/Navbar";
 
 export default function dashboard() {
+  const textMessages = [
+    "Start by reducing carbon footprint, water usage, waste production, and energy usage",
+    "Okay thanks, how can I reduce my carbon footprint?",
+    "You can reduce your carbon footprint by using renewable energy, reducing waste, and using energy-efficient appliances",
+    "Okay, thanks for the information",
+    "You're welcome",
+    "You're welcome",
+    "You're welcome",
+    "You're welcome",
+    "You're welcome",
+    "You're welcome",
+    "You're welcome",
+  ];
   return (
     <div className="h-[1907px] w-full bg-white bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px]">
       {" "}
@@ -13,18 +26,27 @@ export default function dashboard() {
             <div className="absolute h-40 w-40 top-3/4 left-1/3 bg-gradient-radial from-amber-400 via-red-500 to-pink-600 rounded-full mix-blend-multiply filter blur-3xl opacity-80 animate-blob animation-delay-2000 z-0 animation-move-3"></div> */}
         <div className="w-[92%] left-[4%] h-[850px] relative top-[100px]">
           <div className="w-[40%] h-[84%] right-[4%] bg-[#262626] fixed top-[100px] rounded-md p-10 flex-col flex justify-between">
-            <div>
-              <div className="text-white tracking-wide">Chat with buddy</div>
-              <div className="text-6xl text-white tracking-normal font-bold mt-12">
-                So here are some strategies to decrease your companies
-                enviornmental impact
+            <div className="text-white tracking-wide mb-3">Chat with buddy</div>
+
+            <div className="overflow-auto mb-[70px]">
+              <div className="text-4xl text-white tracking-normal font-bold ">
+                <div className="p-4">
+                  {textMessages.map((message, index) => (
+                    <div
+                      key={index}
+                      className={`my-2 p-2 rounded-lg ${index % 2 === 0 ? "text-left ml-auto" : "text-right mr-auto text-xl"}`}
+                    >
+                      <p>{message}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
             <div className="rounded-full bg-white w-[50%] h-auto bottom-10 absolute right-10 items-center p-3 pl-5">
               <textarea
                 rows="1"
                 placeholder="Type here"
-                className="outline-none resize-none flex items-center justify-center"
+                className="outline-none resize-none flex items-center justify-center w-full"
               ></textarea>
             </div>
           </div>
