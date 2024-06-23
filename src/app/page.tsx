45ef41d25bac83eb4bb5e45f00fd99d5e5782c2f
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-06-22 18:34:51 -0700

    page 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,12 @@ export default function Home() {
         <div className="relative flex  items-center justify-center space-y-5 h-full bg-green-dots rounded-xl">
           <div className="pb-8 w-full max-w-2xl space-y-4 ">
             <div className="py-8 space-y-2 text-left">
-              <h1 className="mb-4 text-4xl font-light text-white inset-6">
-                Transforming ESG Insights into Action{" "}
-              </h1>
+              <h1 className="mb-4 text-4xl font-light text-white inset-6">Transforming ESG Insights into Action </h1>
               <div className="flex justify-between items-center">
                 <p className="text-gray-200  max-w-[75%] text-xl">
-                  Input an ESG (Environmental, Social, and Governance) report
-                  for analysis and to talk to a chatbot.
+                  Input an ESG (Environmental, Social, and Governance) report for analysis and to talk to a chatbot.
                 </p>
-                <button className="bg-white px-8 py-4 rounded-3xl text-sm">
-                  try it out
-                </button>
+                <button className="bg-white px-8 py-4 rounded-3xl text-sm">try it out</button>
               </div>
             </div>
             <Image
@@ -34,39 +29,34 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="h-screen flex px-32 flex-col gap-8 bg-white bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px] snap-start py-32">
+      <div className="h-screen flex px-32 flex-col gap-8 snap-start py-16">
         <div className="flex w-full items-center flex-col gap-4">
-          <h1 className="text-7xl  font-normal">Generate Insights With</h1>
-          <h4 className="text-xl text-gray-500 font-medium">
-            Search a company or upload an ESG report
-          </h4>
+          <h1 className="text-6xl  font-normal">Generate Insights With</h1>
+          <h4 className="text-xl text-gray-500 font-medium">Search a company or upload an ESG report</h4>
         </div>
-        <div className="bg-green-dots w-full flex flex-col items-center py-16 rounded-xl">
-          <div className="flex flex-col gap-2 relative -top-4">
+        <div className="bg-green-dots w-full flex flex-col items-center py-12 rounded-xl">
+          <div className="flex flex-col gap-2 relative -top-4 w-1/2">
             <h1 className="text-white  text-2xl">Search a company</h1>
             <div className="flex gap-4 items-center justify-between h-14">
-              <input
-                type="text"
-                className="p-2 px-6 text-xl w-[24rem] h-full rounded-md b-2 outline-black outline-2 outline "
-              />
-              <button className="px-10 py-2 font-light text-xl h-full bg-white rounded-md outline-black outline-2 outline">
-                Next
-              </button>
+              <input type="text" className="p-2 px-6 text-xl w-full h-full rounded-md b-2 outline-black outline-2 outline " />
+              <button className="px-10 py-2 font-light text-xl h-full bg-white rounded-md outline-black outline-2 outline">Next</button>
             </div>
           </div>
         </div>
-        <div className="bg-green-dots w-full flex flex-col items-center py-16 rounded-xl">
-          <div className="flex flex-col gap-2 relative -top-4">
+        <div className="bg-green-dots w-full flex flex-col items-center py-12 rounded-xl">
+          <div className="flex flex-col gap-2 relative -top-4 w-1/2">
             <h1 className="text-white  text-2xl">Upload a File</h1>
-            <div className="flex gap-4 items-center justify-between h-14">
-              <input
-                type="text"
-                className="p-2 px-6 text-xl w-[24rem] h-full rounded-md b-2 outline-black outline-2 outline "
-              />
-              <button className="px-10 py-2 font-light text-xl h-full bg-white rounded-md outline-black outline-2 outline">
-                Next
-              </button>
+            {/* <div className="flex gap-4 items-center justify-between h-14 w-full">
+              <label htmlFor="filePicker" className="w-full h-64 bg-white outline-2">
+                My custom choose file label
+              </label>
+              <input id="filePicker" style={{ visibility: "hidden" }} type={"file"}></input>
+            </div> */}
+            <div className="w-full justify-between my-4 text-white flex">
+              <h1 className="text-lg font-semi-bold">Supported Format: .pdf</h1>
+              <h1 className="text-lg font-semi-bold">Maximum size: 15 mb</h1>
             </div>
+            <button className="px-10 py-2 font-light text-xl  self-end bg-white rounded-md outline-black outline-2 outline w-fit h-14">Next</button>
           </div>
         </div>
       </div>
