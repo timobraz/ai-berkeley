7898dc0d657baecac7252dd30d0ac181d6997b37
Andrew Hwang <pandy430@gmail.com>
2024-06-23 03:58:53 -0700

    Add Loading component and improve UI

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import Markdown from "markdown-to-jsx";
 import { diffChars, diffLines } from "diff";
 import axios from "axios";
+import Loading from "../components/Loading";
 
 const one = "beep boop";
 const other = "beep boob blah";
@@ -11,6 +12,15 @@ const diff = diffLines(one, other);
 console.log(diff);
 
 export default function Upload() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
   const scrollRef = useRef<HTMLDivElement>(null);
   const [messages, setMessages] = useState<string[]>([
     "Generating your markdown report",
@@ -79,49 +89,56 @@ export default function Upload() {
     scrollToBottom();
   }, [messages]);
   return (
-    <main className="w-full bg-white bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px] min-h-screen flex  px-10 gap-8 pt-28 pb-12 ">
-      <div className="flex flex-1  h-[48rem] sticky top-20 ">
-        <div className=" bg-[#262626]  rounded-md px-8 py-4 flex-col flex justify-between  w-full">
-          <div className="text-white tracking-wide my-4 ">
-            <h4 className="my-2">Improve your report</h4>
-            <div className="text-xl text-white tracking-normal font-bold overflow-auto">
-              {messages.length > 0 ? (
-                messages.map((message, index) => (
-                  <div
-                    key={index}
-                    className={`my-2 rounded-lg ${index % 2 === 0 ? "text-left ml-auto text-green-300" : "text-right mr-auto text-xl font-light"}`}
-                  >
-                    <p>{message}</p>
-                  </div>
-                ))
-              ) : (
-                <h2 className="text-green-300">
-                  There are no messages yet. Start chatting!
-                </h2>
-              )}
+    <div>
+      {" "}
+      {loading ? (
+        <Loading />
+      ) : (
+        <main className="w-full bg-white bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px] min-h-screen flex  px-10 gap-8 pt-28 pb-12 ">
+          <div className="flex flex-1  h-[48rem] sticky top-20 ">
+            <div className=" bg-[#262626]  rounded-md px-8 py-4 flex-col flex justify-between  w-full">
+              <div className="text-white tracking-wide my-4 ">
+                <h4 className="my-2">Improve your report</h4>
+                <div className="text-xl text-white tracking-normal font-bold overflow-auto">
+                  {messages.length > 0 ? (
+                    messages.map((message, index) => (
+                      <div
+                        key={index}
+                        className={`my-2 rounded-lg ${index % 2 === 0 ? "text-left ml-auto text-green-300" : "text-right mr-auto text-xl font-light"}`}
+                      >
+                        <p>{message}</p>
+                      </div>
+                    ))
+                  ) : (
+                    <h2 className="text-green-300">
+                      There are no messages yet. Start chatting!
+                    </h2>
+                  )}
 
-              <div ref={scrollRef}></div>
+                  <div ref={scrollRef}></div>
+                </div>
+              </div>
+
+              <div className="rounded-md bg-white  bottom-10 items-center py-2 px-4 pl-4 w-full   mt-4 ">
+                <form
+                  onSubmit={handleSubmit}
+                  className="flex items-center justify-between"
+                >
+                  <input
+                    placeholder="Type here"
+                    className="outline-none resize-none flex items-center justify-center w-full"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  ></input>
+                </form>
+              </div>
             </div>
           </div>
-
-          <div className="rounded-md bg-white  bottom-10 items-center py-2 px-4 pl-4 w-full   mt-4 ">
-            <form
-              onSubmit={handleSubmit}
-              className="flex items-center justify-between"
-            >
-              <input
-                placeholder="Type here"
-                className="outline-none resize-none flex items-center justify-center w-full"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              ></input>
-            </form>
+          <div className="flex-[2] bg-white ring-1 ring-lime-700 outline-offset-8 p-6 rounded-xl [&>*]:my-4 ">
+            <Markdown>{mdx}</Markdown>
           </div>
-        </div>
-      </div>
-      <div className="flex-[2] bg-white ring-1 ring-lime-700 outline-offset-8 p-6 rounded-xl [&>*]:my-4 ">
-        <Markdown>{mdx}</Markdown>
-      </div>
-    </main>
+        </main>
+      )}
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,12 +37,17 @@ export default function Home() {
         <div className="relative flex  items-center justify-center space-y-5 h-full bg-green-dots rounded-xl">
           <div className="pb-8 w-full max-w-[65%] space-y-4 ">
             <div className="py-8 space-y-2 text-left mb-[200px]">
-              <h1 className="mb-4 text-6xl font-light text-white inset-6">Transforming ESG Insights into Action </h1>
+              <h1 className="mb-4 text-6xl font-light text-white inset-6">
+                Transforming ESG Insights into Action{" "}
+              </h1>
               <div className="flex justify-between items-center">
                 <p className="text-gray-200  max-w-[75%] text-xl">
-                  Input an ESG (Environmental, Social, and Governance) report for analysis and to talk to a chatbot.
+                  Input an ESG (Environmental, Social, and Governance) report
+                  for analysis and to talk to a chatbot.
                 </p>
-                <button className="bg-white px-8 py-4 rounded-3xl text-sm mr-14">try it out</button>
+                <button className="bg-white px-8 py-4 rounded-3xl text-sm mr-14">
+                  try it out
+                </button>
               </div>
             </div>
             <Image
@@ -59,13 +64,19 @@ export default function Home() {
       <div className="min-h-screen flex px-32 flex-col gap-4 snap-start pt-16 pb-0">
         <div className="flex w-full items-center flex-col gap-4">
           <h1 className="text-6xl  font-normal">Generate Insights With</h1>
-          <h4 className="text-xl text-gray-500 font-medium">Search a company or upload an ESG report</h4>
+          <h4 className="text-xl text-gray-500 font-medium">
+            Search a company or upload an ESG report
+          </h4>
         </div>
         <div className="bg-dots2 bg-bottom w-full flex flex-col items-center py-12 rounded-lg mt-10">
           <div className="flex flex-col gap-2 relative -top-4 w-1/2">
             <h1 className="text-white  text-2xl">Search a company</h1>
             <div className="flex gap-4 items-center justify-between h-14">
-              <input type="text" placeholder="company e.g. Qualcomm" className="p-2 px-6 text-xl w-full h-full rounded-md b-2  " />
+              <input
+                type="text"
+                placeholder="company e.g. Qualcomm"
+                className="p-2 px-6 text-xl w-full h-full rounded-md b-2  "
+              />
               <Link
                 href="/dashboard"
                 className="px-10 py-2 font-light text-xl h-full bg-white rounded-md text-center items-center flex justify-center"
@@ -83,13 +94,30 @@ export default function Home() {
             <label
               className={clsx(
                 "flex  flex-col items-center justify-center w-full bg-white border-dashed border-3 py-8 space-y-4 border-primary rounded-xl hover:border-[#81D2FF] cursor-pointer transition duration-200",
-                file?.name && "bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px]"
+                file?.name &&
+                  "bg-[radial-gradient(#B1B1B1_1px,transparent_1px)] [background-size:48px_48px]"
               )}
             >
-              {!file ? <IconUpload className="text-primary w-12 h-12" /> : <IconFileAnalytics className="text-primary w-12 h-12" />}
-              <h3 className="text-2xl font-bold">{file?.name ?? "File Select"}</h3>
-              {!file && <p className=" text-center">Drag and drop a file here or click to select a file from your device.</p>}
-              <input type="file" className="sr-only" onChange={handleChange} accept=".pdf, .csv" />
+              {!file ? (
+                <IconUpload className="text-primary w-12 h-12" />
+              ) : (
+                <IconFileAnalytics className="text-primary w-12 h-12" />
+              )}
+              <h3 className="text-2xl font-bold">
+                {file?.name ?? "File Select"}
+              </h3>
+              {!file && (
+                <p className=" text-center">
+                  Drag and drop a file here or click to select a file from your
+                  device.
+                </p>
+              )}
+              <input
+                type="file"
+                className="sr-only"
+                onChange={handleChange}
+                accept=".pdf, .csv"
+              />
             </label>
             <div className="w-full justify-between mt-1 mb-5 text-white flex">
               <h1 className="text-lg font-semi-bold">Supported Format: .pdf</h1>
@@ -107,7 +135,9 @@ export default function Home() {
       <div className="min-h-screen flex px-32 flex-col gap-4 snap-start pt-8">
         <div className="flex w-full items-center flex-col gap-4">
           <h1 className="text-6xl  font-normal">Generate Your ESG Report</h1>
-          <h4 className="text-xl text-gray-500 font-medium">Input Data to Generate Your ESG Report</h4>
+          <h4 className="text-xl text-gray-500 font-medium">
+            Input Data to Generate Your ESG Report
+          </h4>
         </div>
         <form className="flex flex-row gap-10 mt-4" action="/generate">
           <div className="bg-dots4 bg-bottom w-full flex flex-col items-center rounded-lg">
@@ -119,66 +149,126 @@ export default function Home() {
               ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-0  flex p-5 items-center justify-between">
-              <p className="text-white opacity-100 text-xl">Percentage Grid Electricity</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <p className="text-white opacity-100 text-xl">
+                Percentage Grid Electricity
+              </p>
+              <input
+                name="percentage_grid_electricity"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-30  flex p-5 items-center justify-between">
               {" "}
-              <p className="text-white opacity-100 text-xl">Percentage Renewable</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <p className="text-white opacity-100 text-xl">
+                Percentage Renewable
+              </p>
+              <input
+                name="percentage_renewable"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-0  flex p-5 items-center justify-between">
-              <p className="text-white opacity-100 text-xl">Total Water Withdrawn</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <p className="text-white opacity-100 text-xl">
+                Total Water Withdrawn
+              </p>
+              <input
+                name="total_water_withdrawn"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-30  flex p-5 items-center justify-between">
-              <p className="text-white opacity-100 text-xl">Total Water Consumed</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <p className="text-white opacity-100 text-xl">
+                Total Water Consumed
+              </p>
+              <input
+                name="total_water_consumed"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-0  flex p-5 items-center justify-between">
               <p className="text-white opacity-100 text-xl">Waste Generated</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <input
+                name="waste_generated"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-30 rounded-b-md  flex p-5 items-center justify-between">
               <p className="text-white opacity-100 text-xl">Waste Landfilled</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <input
+                name="waste_landfilled"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-0 rounded-t-md  flex p-5 items-center justify-between">
               <p className="text-white opacity-100 text-xl">GGE Scope 1</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <input
+                name="scope_1"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-30  flex p-5 items-center justify-between">
               <p className="text-white opacity-100 text-xl">GGE Scope 2</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <input
+                name="scope_2"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
             <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-0  flex p-5 items-center justify-between">
               <p className="text-white opacity-100 text-xl">GGE Scope 3</p>
-              <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+              <input
+                name="scope_3"
+                className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+              ></input>
             </div>
           </div>
           <div className="w-full h-full flex flex-col gap-10">
             <div className="bg-dots5 w-full flex flex-col items-center rounded-lg h-fit">
               <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-0 flex p-5 items-center justify-between">
-                <p className="text-white opacity-100 text-xl">GGE DC Total Emissions</p>
-                <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+                <p className="text-white opacity-100 text-xl">
+                  GGE DC Total Emissions
+                </p>
+                <input
+                  name="total_emissions"
+                  className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+                ></input>
               </div>
               <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-30  flex p-5 items-center justify-between">
-                <p className="text-white opacity-100 text-xl">GGE Purchased Goods</p>
-                <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+                <p className="text-white opacity-100 text-xl">
+                  GGE Purchased Goods
+                </p>
+                <input
+                  name="purchased_goods"
+                  className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+                ></input>
               </div>
               <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-0  flex p-5 items-center justify-between">
-                <p className="text-white opacity-100 text-xl">GGE Capital Goods</p>
-                <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"></input>
+                <p className="text-white opacity-100 text-xl">
+                  GGE Capital Goods
+                </p>
+                <input
+                  name="capital_goods"
+                  className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white"
+                ></input>
               </div>
               <div className="bg-[#D9D9D9] w-full h-1/7 bg-opacity-30 rounded-b-md  flex p-5 items-center justify-between">
                 {" "}
                 <p className="text-white opacity-100 text-xl">Air Emissions </p>
-                <input className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white rounded-b-md"></input>
+                <input
+                  name="air_emissions"
+                  className="bg-transparent border-b-2 resize-none items-center flex justify-center text-xl outline-none text-white rounded-b-md"
+                ></input>
               </div>
             </div>
-            <div className="bg-dots3 w-full flex flex-col items-center rounded-lg h-auto p-10 text-white text-xl gap-5" id="generate">
+            <div
+              className="bg-dots3 w-full flex flex-col items-center rounded-lg h-auto p-10 text-white text-xl gap-5"
+              id="generate"
+            >
               <p>Give a brief description of the company</p>
-              <textarea rows={5} className="w-full bg-transparent outline-none resize-none text-white text-xl border-b-2"></textarea>
+              <textarea
+                name="description"
+                rows={5}
+                className="w-full bg-transparent outline-none resize-none text-white text-xl border-b-2"
+              ></textarea>
             </div>
             <div className="flex justify-end">
               <button className="bg-dots2 w-full flex flex-col items-center rounded-lg h-1/2 p-6 text-white text-xl gap-5 cursor-pointer">
